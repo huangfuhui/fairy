bdf61265b196b9c268f90040d62f64d904feb005
huangfuhui <huangfuhui@outlook.com>
2017-12-02 00:52:08 +0800

    首页控制器

---
+++ app/Http/Controllers/Mobile/Home/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * File: IndexController.php
+ *
+ * User: huangfuhui
+ * Date: 2017/12/2 0:50
+ * Email: huangfuhui@outlook.com
+ */
+
+namespace App\Http\Controllers\Mobile\Home;
+
+
+class IndexController
+{
+
+}
