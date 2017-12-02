9b58e52ffc25b0ebd13b3f35d9f87b5bf24b8bba
huangfuhui <huangfuhui@meimeifa.com>
2017-12-02 14:40:49 +0800

    AntService

---
+++ app/Services/AntService.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @FileName AntService.php
+ *
+ * @Author: huangfuhui
+ * @Date: 2017/12/2 下午2:28
+ * @Email: huangfuhui@meimeifa.com
+ */
+
+namespace App\Services;
+
+
+use GuzzleHttp\Client;
+
+class AntService
+{
+    private $client = null;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+}
