bf4b58a13d1373d034df6b223b54ed5d279429d9
huangfuhui <huangfuhui@meimeifa.com>
2017-12-02 13:55:34 +0800

    引入ide-helper

--- config/database.php
+++ config/database.php
@@ -50,8 +50,8 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => 'fairy_',
-            'strict' => true,
-            'engine' => null,
+            'strict' => false,
+            'engine' => 'innodb',
         ],
 
         'pgsql' => [
