a9024b9aed67ca8658029d16dbb56388cb833abb
huangfuhui <huangfuhui@outlook.com>
2017-12-03 21:41:15 +0800

    更新配置文件

---
+++ config/ant.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * File: ant.php
+ *
+ * User: huangfuhui
+ * Date: 2017/12/3 11:01
+ * Email: huangfuhui@outlook.com
+ */
+
+return [
+    'base_uri'     => 'http://www.biquge5200.com',
+    'cover_dir'    => 'covers/',
+    'cover_suffix' => 'jpg',
+
+    'pattern' => [
+        'book' => [
+            'name'             => '[<meta property="og:title" content="(.*)"/>]',
+            'author'           => '[<meta property="og:novel:author" content="(.*)"/>]',
+            'profile'          => '[<meta property="og:description" content="(.*)"/>]',
+            'type'             => '[<meta property="og:novel:category" content="(.*)"/>]',
+            'cover'            => '[<meta property="og:image" content="(.*)"/>]',
+            'status'           => '[<meta property="og:novel:status" content="(.*)"/>]',
+            'chapter_contents' => '[<dt>.*正文</dt>(.*)</dl>]s',
+            'chapter_list'     => '[<dd><a href="(.*)">(.*)</a></dd>]',
+            'content'          => '[<div id="content">(.*?)</div>]s',
+        ],
+    ],
+];
