72973612c2461badc49aec8c39ab1cf3bebd3f04
huangfuhui <huangfuhui@meimeifa.com>
2017-12-06 12:45:20 +0800

    修改错误任务执行操作

--- app/Listeners/BookInitLister.php
+++ app/Listeners/BookInitLister.php
@@ -16,13 +16,6 @@ class BookInitLister implements ShouldQueue
     public $queue = 'book-init-event';
 
     /**
-     * 最大重试次数
-     *
-     * @var int
-     */
-    public $tries = 3;
-
-    /**
      * Create the event listener.
      */
     public function __construct()
@@ -33,6 +26,7 @@ class BookInitLister implements ShouldQueue
      * Handle the event.
      *
      * @param  AntEvent $event
+     *
      * @return mixed
      */
     public function handle(AntEvent $event)
@@ -44,17 +38,25 @@ class BookInitLister implements ShouldQueue
 
         if ($ant instanceof Content) {
             $chapterName = $ant->getChapterName();
-            echo "开始初始化书籍章节内容.   book_id: $bookId, chapter_name: $chapterName" . PHP_EOL;
+            echo "[第 $attempts 次]开始初始化书籍章节内容.   book_id: $bookId, chapter_name: $chapterName" . PHP_EOL;
             $res = Ant::initContent($bookId, $chapterName, $requestUrl);
-            $res ? $msg = "第 $attempts 次初始化成功." : $msg = "第 $attempts 次初始化失败";
+            $res ? $msg = "[第 $attempts 次]初始化成功." : $msg = "[第 $attempts 次]初始化失败. http_code：" . Ant::getErrMsg();
             echo $msg . PHP_EOL;
+
+            if (!$res) {
+                $this->release($attempts);
+            }
 
             return false;
         } elseif ($ant instanceof Chapter) {
-            echo "开始初始化书籍章节信息.   book_id: $bookId" . PHP_EOL;
+            echo "[第 $attempts 次]开始初始化书籍章节信息.   book_id: $bookId" . PHP_EOL;
             $res = Ant::initChapter($bookId, $requestUrl);
-            $res ? $msg = "第 $attempts 次初始化成功." : $msg = "第 $attempts 次初始化失败";
+            $res ? $msg = "[第 $attempts 次]初始化成功." : $msg = "[第 $attempts 次]初始化失败. http_code：" . Ant::getErrMsg();
             echo $msg . PHP_EOL;
+
+            if (!$res) {
+                $this->release($attempts);
+            }
 
             return false;
         }
