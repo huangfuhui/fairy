4710f704ec1d85416f5f3d3d357cfdac33db8fc8
huangfuhui <huangfuhui@meimeifa.com>
2017-12-12 17:34:56 +0800

    前端常量配置

---
+++ config/web.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @FileName web.php
+ *
+ * @Author: huangfuhui
+ * @Date: 2017/12/12 下午3:42
+ * @Email: huangfuhui@meimeifa.com
+ */
+
+return [
+    'meta'  => [
+        'keywords'    => '小精灵小说',
+        'description' => '手机小说 无广告 无弹窗',
+    ],
+    'title' => '小精灵小说',
+];
